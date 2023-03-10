42bc910b033e059721bfcf5f41e838d800d39655
Deiv-Dev <nomaxas@gmail.com>
2023-03-10 12:35:20 +0000

    used wrong api call for meteo.lt, changet to correct and redid data filtering that result of get data will show just 3 next days that displays name of city, date of day and most likely weather in day

--- app/Http/Controllers/WeatherController.php
+++ app/Http/Controllers/WeatherController.php
@@ -2,7 +2,8 @@
 namespace App\Http\Controllers;
 use Illuminate\Support\Facades\Http;
 use Validator;
-
+use DateTime;
+use DateTimeZone;
 
 use Illuminate\Http\Request;
 
@@ -10,61 +11,105 @@ class WeatherController extends Controller
 {
     public function index()
     {
-        try {
+        try
+        {
             $url = "https://api.meteo.lt/v1/places";
             $response = Http::get($url);
             $places = $response->json();
             $cityNames = array();
-            foreach ($places as $place) {
-                if (!in_array($place['name'], $cityNames)) {
+            foreach ($places as $place)
+            {
+                if (!in_array($place['name'], $cityNames))
+                {
                     $cityNames[] = $place['name'];
                 }
             }
             return view('welcome')->with('cityNames', $cityNames);
-        } catch(Exception $e) {
-            return response()->json(["error" => $e->getMessage()], 500);
         }
-       
-        
+        catch(Exception $e)
+        {
+            return response()->json(["error" => $e->getMessage() ], 500);
+        }
     }
 
     public function processForm(Request $request)
     {
 
-        $validatet = Validator::make($request->all(),[
-            'city' => 'required|string|max:25',
-        ]);
+        $validatet = Validator::make($request->all() , ['city' => 'required|string|max:25', ]);
 
-        if ($validatet->fails()) {
-            return response()->json(["error" => "Text input is not correct"], 422);
-        } else {
+        if ($validatet->fails())
+        {
+            return response()
+                ->json(["error" => "Text input is not correct"], 422);
+        }
+        else
+        {
             $city = $request->input('city');
             $city = strtolower($city);
             $results = [];
         }
-        
-        for ($i = 0;$i < 3;$i++)
+
+        try
         {
-            try
+            $url = "https://api.meteo.lt/v1/places/$city/forecasts/long-term";
+            $response = Http::get($url);
+            $weather = $response->json();
+            $currentDate = new DateTime('now', new DateTimeZone('UTC'));
+            $lastDate = null;
+
+            // Loop through each day in the forecast
+            foreach ($weather['forecastTimestamps'] as $forecast)
             {
-                $date = date('Y-m-d', strtotime("-{$i} day"));
-                $url = "https://api.meteo.lt/v1/stations/$city-ams/observations/$date";
-                $response = Http::get($url);
-                $weather = $response->json();
-                $results[] = $weather;
+                $forecastDate = new DateTime($forecast['forecastTimeUtc']);
+                $diff = $forecastDate->diff($currentDate);
+
+                // Check if the forecast is within the next 3 days
+                if ($diff->days <= 3)
+                {
+                    $dateString = $forecastDate->format('Y-m-d');
+                    if ($dateString !== $lastDate)
+                    {
+                        $dayResults = ['name' => $weather['place']['name'], 'forecastTimeUtc' => $forecast['forecastTimeUtc']];
+                        
+                        // Loop through each forecast for this day and add condition codes to array
+                        foreach ($weather['forecastTimestamps'] as $forecastOfDay)
+                        {
+                            $daysConditionCount['conditionCodes'][] = $forecastOfDay['conditionCode'];
+                        }
+
+                        // Get the most common condition code for that day
+                        $conditionCounts = array_count_values($daysConditionCount['conditionCodes']);
+                        $mostCommonConditionCode = key($conditionCounts);
+
+                        // Add the most common condition code to the day's results
+                        $dayResults['conditionCode'] = $mostCommonConditionCode;
+                        $results[] = $dayResults;
+
+                        $lastDate = $dateString;
+                    }
+                }
             }
-            catch(Exception $e)
-            {
-                return response()->json(["error" => $e->getMessage()], 500);
-            }
+
+            // Return only the next 3 days (excluding the current day)
+            $responseResults = array_slice($results, 1, 3);
+            dd($responseResults);
+            
+        }
+        catch(Exception $e)
+        {
+            return response()->json(["error" => $e->getMessage() ], 500);
         }
 
-        if (empty($results)) {
+        if (empty($results))
+        {
             return response()->json(["error" => "Failed to fetch weather data"], 500);
-        } else {
+        }
+        else
+        {
             dd($results);
         }
-        
+
         return view('welcome');
     }
 }
+
