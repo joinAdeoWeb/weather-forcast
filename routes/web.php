218d25f19775c694c78b30493faa2f1b748797d7
Deiv-Dev <nomaxas@gmail.com>
2023-03-13 17:46:51 +0000

    fixed rout for processForm

--- routes/web.php
+++ routes/web.php
@@ -14,4 +14,4 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', ["App\Http\Controllers\WeatherController", 'index'])->name('index');
-Route::post('/', ["App\Http\Controllers\WeatherController", 'processForm'])->name('processForm');
+Route::post('/submit-form', ["App\Http\Controllers\WeatherController", 'processForm'])->name('processForm');
