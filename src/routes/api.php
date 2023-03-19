beb791be089ce935881137110804e137ba0f3977
Deiv-Dev <nomaxas@gmail.com>
2023-03-19 16:58:24 +0000

    deleted some of the files that ar not used

--- src/routes/api.php
+++ src/routes/api.php
@@ -13,5 +13,3 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
-
-Route::post('clothes-recomendations', 'App\Http\Controllers\WeatherController@processForm');
